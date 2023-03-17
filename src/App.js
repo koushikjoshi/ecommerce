fef7807d6ba0dabe906057aa0f43234b537eba19
koushikjoshi <39240894+koushikjoshi@users.noreply.github.com>
2023-03-17 16:17:44 +0530

    filter UI done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import Filters from "./components/Filters.styled";
 import Navbar from "./components/Navbar.styled";
 
 function App() {
@@ -6,7 +7,14 @@ function App() {
     <div className="App">
       <Navbar />
       <div className="products-filters">
-        <p>Products:</p>
+        <div className="filters-div">
+          <p>Filter Products:</p>
+          <Filters />
+        </div>
+        <div className="products-div">
+          <p>Our Products:</p>
+          {/* products will be visible here */}
+        </div>
       </div>
     </div>
   );
--- src/components/Filters.styled.jsx
+++ src/components/Filters.styled.jsx
@@ -1,8 +1,94 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 
 const Filters = () => {
-  return <div></div>;
+  const [rating, setRating] = useState(0);
+  const [price, setPrice] = useState(0);
+
+  const handleStarClick = (value) => {
+    setRating(value);
+  };
+
+  const handlePriceChange = (event) => {
+    setPrice(event.target.value);
+  };
+
+  return (
+    <FilterWrapper>
+      <FilterSection>
+        <FilterTitle>Price</FilterTitle>
+        <PriceWrapper>
+          <PriceLabel>0</PriceLabel>
+          <FilterInput
+            type="range"
+            min="0"
+            max="1000"
+            value={price}
+            onChange={handlePriceChange}
+          />
+          <PriceLabel>1000</PriceLabel>
+        </PriceWrapper>
+        <SelectedPrice>Selected Price: {price}</SelectedPrice>
+      </FilterSection>
+      <FilterSection>
+        <FilterTitle>Rating</FilterTitle>
+        <StarWrapper>
+          {[1, 2, 3, 4, 5].map((value) => (
+            <Star
+              key={value}
+              onClick={() => handleStarClick(value)}
+              filled={rating >= value}
+            >
+              ★
+            </Star>
+          ))}
+        </StarWrapper>
+      </FilterSection>
+    </FilterWrapper>
+  );
 };
+
+const FilterWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+`;
+
+const FilterSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+`;
+
+const FilterTitle = styled.h4`
+  margin: 0;
+  font-weight: 500;
+`;
+
+const PriceWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const PriceLabel = styled.span``;
+
+const SelectedPrice = styled.p`
+  margin: 0;
+  font-size: 0.9rem;
+`;
+
+const FilterInput = styled.input`
+  width: 100%;
+`;
+
+const StarWrapper = styled.div`
+  display: flex;
+  gap: 0.25rem;
+`;
+
+const Star = styled.span`
+  cursor: pointer;
+  color: ${(props) => (props.filled ? "#FFD700" : "#C0C0C0")};
+`;
 
 export default Filters;
